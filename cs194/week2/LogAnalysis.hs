0bd842a1519e4e288db589261b238a4a1fdb1eb9
Jesus <andrei.alexandru@ubisoft.com>
2016-02-02 17:02:33 +0200

    Refactor first exercise

--- cs194/week2/LogAnalysis.hs
+++ cs194/week2/LogAnalysis.hs
@@ -12,71 +12,29 @@ module LogAnalysis where
 
 import Log
 
+-- | @isUInt s@ returns true if the string @s@ is an unsigned integer, false otherwise.
 isUInt :: String -> Bool
 isUInt [] = False
 isUInt (x:[]) = x `elem` ['0'..'9']
 isUInt (x:xs) = x `elem` ['0'..'9'] && isUInt xs
 
-{-
-begWith :: [String] -> String -> Bool
-begWith [] _ = False
-begWith (x:xs) toMatch = x == toMatch
+-- | @parseMessage' w@ parse the word list @w@ and returns a LogMessage
+parseMessage' :: [String] -> LogMessage
+parseMessage' ("I":y:xs)
+  | isUInt y  = LogMessage Info (read y) (unwords xs)
+  | otherwise = Unknown (unwords (y:xs))
+parseMessage' ("W":y:xs)
+  | isUInt y  = LogMessage Warning (read y) (unwords xs)
+  | otherwise = Unknown (unwords (y:xs))
+parseMessage' ("E":y:z:xs)
+  | isUInt y && isUInt z = LogMessage (Error (read y))(read z) (unwords xs)
+  | otherwise = Unknown (unwords (y:xs))
+parseMessage' xs = Unknown (unwords xs)
 
-getBegUInts :: [String] -> [Int]
-getBegUInts [] = []
-getBegUInts (x:xs)
-  | isUInt x  = (read x :: Int) : getBegUInts xs
-  | otherwise = []
+-- | @parseMessage s@ parse the string @s@ and returns a LogMessage
+parseMessage :: String ->LogMessage
+parseMessage s = parseMessage' (words s)
 
-begWithStringUInt :: [String] -> String -> Bool
-begWithStringUInt [] _ = False
-begWithStringUInt (_:[]) _ = False
-begWithStringUInt (str : int : xs) toMatch = str == toMatch && isUInt int 
--}
-
-isInfo :: [String] -> Bool
-isInfo [] = False
-isInfo (_:[]) = False
-isInfo (x:y:_) = x == "I" && isUInt y
-
-isWarning :: [String] -> Bool
-isWarning [] = False
-isWarning (_:[]) = False
-isWarning (x:y:_) = x == "W" && isUInt y
-
-isError :: [String] -> Bool
-isError [] = False
-isError (_:[]) = False
-isError (_:_:[]) = False
-isError (x:y:z:_) = x == "E" && isUInt y && isUInt z
-
-testIfValid :: String -> Bool
-testIfValid xs = let w = words xs in isInfo w || isWarning w || isError w
-
-getType :: [String] -> MessageType
-getType xs
-  | isInfo xs = Info
-  | isWarning xs = Warning
-  | isError xs = (Error (read (xs !! 1)))
-
-getTimeStamp :: [String] -> TimeStamp
-getTimeStamp xs
-  | isError xs = read $ xs !! 2
-  | otherwise  = read $ xs !! 1
-
-getMsg :: [String] -> String
-getMsg xs
-  | isError xs = unwords (drop 3 xs)
-  | otherwise  = unwords (drop 2 xs)
-
-parseMessage :: String -> LogMessage
-parseMessage xs 
-  | testIfValid xs = let w = words xs in LogMessage (getType w) (getTimeStamp w) (getMsg w)
-  | otherwise      = Unknown xs
-
-parseLines :: [String] -> [LogMessage]
-parseLines [] = []
-parseLines (x:xs) = parseMessage x : parseLines xs
-
+-- | @parse s@ parse the text @s@ and returns a list of LogMessages
 parse :: String -> [LogMessage]
-parse xs = parseLines . lines $ xs
+parse xs = map parseMessage (lines xs)
