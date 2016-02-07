ca8442b945110be783bcd4acc075039eb60c592c
Andrei Alexandru <alexandruandrei91@gmail.com>
2016-02-07 18:22:23 +0200

    Add localMaxima function. (W3E2)

--- cs194/week3/Golf.hs
+++ cs194/week3/Golf.hs
@@ -13,8 +13,8 @@ module Golf where
 -- | @skips xs@ takes the list @xs@ and outputs a list of lists, by this rule:
 -- | the first list in the output is the same as the input; the second list
 -- | in the output contains every second element from the input, the nth list
--- | in the output contains every nth element from the input, for example:
--- | skips "hello!" == ["hello!", "el!", "l!", "l", "o", "!"]
+-- | in the output contains every nth element from the input.
+-- | Example: skips "hello!" == ["hello!", "el!", "l!", "l", "o", "!"]
 -- |
 -- | Implementation details: the input list @xs@ is zipped with a list of integers
 -- | starting from 1, resulting a list of touples of type [(Int, a)]. Using a
@@ -34,3 +34,20 @@ skips :: [a] -> [[a]]
 skips xs = map (\ x ->
                map (snd) (filter (\ x' -> fst x' `mod` x == 0) (zip [1..] xs)))
                [1..length xs]
+
+-- | @localMaxima xs@ takes the list @xs@ and returns a list where each element
+-- | is strictly greater than both the elements immediately before and after it.
+-- | Example: localMaxima [2, 4, 3, 5] == [4]
+-- | 
+-- | Implementation details: the input list @xs@ is zipped in a list touples with
+-- | 3 elements, each element containing 3 the neighbouring numbers from the list.
+-- | A filter is applied on this list of touples, outputing a list which contains
+-- | only touples with the local maxima (where the middle element is greater than
+-- | the two sorrounding elements). Finally, using a map, a list is constructed by
+-- | extracting the middle element from each touple.
+-- | Example: [2, 4, 3, 5] --zip--> [(2, 4, 3), (4, 3, 5)] --filter--> [(2, 4, 3)]
+-- | --map--> [4]
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs = map (\ (_, b, _) -> b)
+                     (filter (\(a, b, c) -> a<b && b>c)
+                            (zip3 xs (drop 1 xs) (drop 2 xs)))
