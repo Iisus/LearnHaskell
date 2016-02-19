e6653505812ec8166a0126b42901e51c6465c528
unknown <andrei.alexandru@CRA-WKS-PR112.ubisoft.org>
2016-02-19 17:06:09 +0200

    Change original functions names

--- cs194/week4/HigherOrder.hs
+++ cs194/week4/HigherOrder.hs
@@ -11,7 +11,7 @@
 module HigherOrder where
 
 
-fun1Orig :: [Integer] -> Integer
+fun1' :: [Integer] -> Integer
 fun1Orig [] = 1
 fun1Orig (x:xs)
   | even x    = (x-2) * fun1Orig xs
@@ -21,7 +21,7 @@ fun1 :: [Integer] -> Integer
 fun1 = product . map (subtract 2) . filter even
 
 
-fun2Orig :: Integer -> Integer
+fun2' :: Integer -> Integer
 fun2Orig 1 = 0
 fun2Orig n | even n    = n + fun2Orig (n `div` 2)
            | otherwise = fun2Orig (3 * n + 1)
