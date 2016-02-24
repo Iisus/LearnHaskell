cfe3171483a2d18b1c5a464599d94eb10820786a
Jesus <andrei.alexandru@ubisoft.com>
2016-02-24 16:13:59 +0200

    fix Ex3, add Exercise 4, Week 5

--- cs194/week5/Calc.hs
+++ cs194/week5/Calc.hs
@@ -26,6 +26,7 @@ evalStr str = case parseExp Lit Add Mul str of
               Just expr -> Just (eval expr)
               Nothing   -> Nothing
 
+
 -- Exercise 3 --
 class Expr a where
   lit :: Integer -> a
@@ -34,5 +35,38 @@ class Expr a where
 
 instance Expr ExprT where
   lit = Lit
-  add y = Add
-  mul y = Mul
+  add = Add
+  mul = Mul
+
+
+-- Exercise 4 --
+instance Expr Integer where
+  lit x = x
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (0<)
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = lit (max x y)
+  mul (MinMax x) (MinMax y) = lit (min x y)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+instance Expr Mod7 where
+  lit x = Mod7 (x `mod` 7)
+  add (Mod7 x) (Mod7 y) = lit (x+y)
+  mul (Mod7 x) (Mod7 y) = lit (x*y)
+
+-- tests
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExp :: Maybe Integer
+testBool    = testExp :: Maybe Bool
+testMinMax  = testExp :: Maybe MinMax
+testMod7    = testExp :: Maybe Mod7
