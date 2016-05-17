f991f87f23a46823bc82cd93d7ee742a1b022d0d
Andrei <alexandruandrei91@gmail.com>
2016-05-18 00:31:06 +0300

    W6 E3,4,5 (5 is not finished)

--- cs194/week6/Lazy.hs
+++ cs194/week6/Lazy.hs
@@ -25,3 +25,33 @@ fib' a b = a : fib' b (a+b)
 
 fibs2 :: [Integer]
 fibs2 = fib' 0 1
+
+-- Exercise 3 --
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+instance Show a => Show (Stream a) where
+  show stream = show . take 20 $ streamToList stream
+
+-- Exercise 4 --
+streamRepeat :: a -> Stream a
+streamRepeat x = (Cons x (streamRepeat x))
+
+streamMap :: (a -> a) -> Stream a -> Stream a
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f seed = Cons seed (streamFromSeed f (f seed))
+
+-- Exercise 5 --
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x xs1) (Cons _ xs2) = Cons x
+                                            . interleaveStreams xs2 $ xs1
+
+ruler :: Stream Integer
+ruler = nats -- TODO
